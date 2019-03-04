789eefda79770ebba1201481047c933da9356cf1
Glen <glenmccann2@gmail.com>
2019-03-03 20:47:47 -0500

    Updates

---
+++ precache-manifest.ffd420b3d361a3dfd43fe69f321cb447.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b49c07ff64ae0ee94f80c96b87ec6857",
+    "url": "/project05/static/media/story-background.b49c07ff.jpg"
+  },
+  {
+    "revision": "d1dcfe99a9abea436ee8",
+    "url": "/project05/static/js/runtime~main.d1dcfe99.js"
+  },
+  {
+    "revision": "0bdc50b0792e3926a47e",
+    "url": "/project05/static/js/main.0bdc50b0.chunk.js"
+  },
+  {
+    "revision": "093252b436c67eb9eb98",
+    "url": "/project05/static/js/2.093252b4.chunk.js"
+  },
+  {
+    "revision": "0bdc50b0792e3926a47e",
+    "url": "/project05/static/css/main.f1e02036.chunk.css"
+  },
+  {
+    "revision": "7a8ca8152a28bb274b90b6cc6e6fefdb",
+    "url": "/project05/index.html"
+  }
+];
